acece29d78e48fccbdb882b4b3ddbbc7b33a143d
Pedro Herrera <alfonzzo7@gmail.com>
2018-06-19 15:57:16 +0200

    primer commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { AngularFirestore } from 'angularfire2/firestore';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,52 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  lat: number;
+  lng: number;
+
+  init = false;
+
+  taxistas: Taxista[] = [];
+  siguiendoA:string = null;
+  siguiendoNombre:string = null;
+
+  constructor(private adDB: AngularFirestore){
+    adDB.collection('usuarios').valueChanges()
+        .subscribe((data:Taxista[]) => {
+          this.taxistas = data;
+          if(!this.init){
+            this.lat = data[0].lat;
+            this.lng = data[0].lng;
+            this.init = true;
+          }
+
+          if(this.siguiendoA){
+            data.forEach(taxista => {
+              if(taxista.clave == this.siguiendoA){
+                this.lat = taxista.lat;
+                this.lng = taxista.lng;
+              }
+            });
+          }
+        });
+  }
+
+  seguir(taxista:Taxista){
+    this.siguiendoA = taxista.clave;
+    this.siguiendoNombre = taxista.nombre;
+    this.lat = taxista.lat;
+    this.lng = taxista.lng;
+  }
+
+  dejarDeSeguir(){
+    this.siguiendoA = null;
+    this.siguiendoNombre = null;
+  }
+}
+
+interface Taxista{
+  nombre:string;
+  clave:string;
+  lat:number;
+  lng:number;
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,14 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-
 import { AppComponent } from './app.component';
+
+//Maps
+import { AgmCoreModule } from '@agm/core';
+
+//Firebase
+import { AngularFireModule } from 'angularfire2';
+import { AngularFirestoreModule } from 'angularfire2/firestore';
 
 
 @NgModule({
@@ -10,7 +16,19 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AgmCoreModule.forRoot({
+      apiKey: 'AIzaSyAU9gZ1W9Wo22LHyVq_Dcx7g9ix8lJT33g'
+    }),
+    AngularFireModule.initializeApp({
+      apiKey: 'AIzaSyBZb3MkhN4bDEConUQwRxms8mFc_jEqzAo',
+      authDomain: 'trackertaxis.firebaseapp.com',
+      databaseURL: 'https://trackertaxis.firebaseio.com',
+      projectId: 'trackertaxis',
+      storageBucket: 'trackertaxis.appspot.com',
+      messagingSenderId: '117646870934'
+    }),
+    AngularFirestoreModule
   ],
   providers: [],
   bootstrap: [AppComponent]
